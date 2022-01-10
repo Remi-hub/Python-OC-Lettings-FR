0b98716b9e60bdef5eceade17b9eb5a1e34e012b
Remi Krommenaker <remi.krommenaker@gmail.com>
2022-01-10 18:36:49 +0100

    updated requirements.txt

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -108,9 +108,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_FILE_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, '/oc_lettings_site/static/')
+STATIC_FILE_ROOT = os.path.join(BASE_DIR, "/oc_lettings_site/static/")
+STATIC_URL = "/oc_lettings_site/static/"
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),

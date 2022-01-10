07d864af2116ecffe04c4ee5224f4dab08e80dd0
Remi Krommenaker <remi.krommenaker@gmail.com>
2022-01-10 14:31:12 +0100

    Installed Sentry

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,4 +1,6 @@
 import os
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -117,3 +119,18 @@ STATICFILES_DIRS = (
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
+
+
+sentry_sdk.init(
+    dsn="https://affd67ca28f04f4387a0b8b08bf2d3ea@o1085754.ingest.sentry.io/6141975",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
-
 from . import views
+from django.urls import path
+
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("lettings/", include("lettings.urls", namespace="lettings")),
     path("profiles/", include("profiles.urls", namespace="profiles")),
     path("admin/", admin.site.urls),
+    path('sentry-debug/', trigger_error),
 ]
